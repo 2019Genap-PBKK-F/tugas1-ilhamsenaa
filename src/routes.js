e9a9508b2ebdf6844fe0d36cc7aed80672c96a17
ilhamsenaa <55700487+ilhamsenaa@users.noreply.github.com>
2020-05-09 21:47:26 +0700

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,16 +5,25 @@ import NotFoundView from './components/404.vue'
 // Import Views - Dash
 import DashboardView from './components/views/Dashboard.vue'
 import TablesView from './components/views/Tables.vue'
+import DosenView from './components/views/Dosen.vue'
+import PenelitianView from './components/views/Penelitian.vue'
+import AbmasView from './components/views/Abmas.vue'
+import PublikasiView from './components/views/Publikasi.vue'
+import DataDasarView from './components/views/DataDasar.vue'
+import AspekView from './components/views/Aspek.vue'
+import MasterIndikatorView from './components/views/MasterIndikator.vue'
+import JenisSatkerView from './components/views/JenisSatker.vue'
+import PeriodeView from './components/views/Periode.vue'
+import IndikatorPeriodeView from './components/views/IndikatorPeriode.vue'
+import SatuanKerjaView from './components/views/SatuanKerja.vue'
+import CapaianUnitView from './components/views/Capaian_Unit.vue'
+import IndikatorSatuanKerjaView from './components/views/Indikator_SatuanKerja.vue'
+import IndikatorSatuanKerjaLogView from './components/views/Indikator_SatuanKerja_Log.vue'
 import TasksView from './components/views/Tasks.vue'
 import SettingView from './components/views/Setting.vue'
 import AccessView from './components/views/Access.vue'
 import ServerView from './components/views/Server.vue'
 import ReposView from './components/views/Repos.vue'
-import ExcelView from './components/views/Excel.vue'
-import DataDasarView from './components/views/DataDasar.vue'
-import KategoriUnitView from './components/views/KategoriUnit.vue'
-import UnitView from './components/views/Unit.vue'
-import CapaianUnitView from './components/views/Capaian_Unit.vue'
 
 // Routes
 const routes = [
@@ -36,6 +45,76 @@ const routes = [
         path: 'tables',
         component: TablesView,
         name: 'Tables',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'Dosen',
+        component: DosenView,
+        name: 'Dosen',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'Penelitian',
+        component: PenelitianView,
+        name: 'Penelitian',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'Abmas',
+        component: AbmasView,
+        name: 'Abmas',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'Publikasi',
+        component: PublikasiView,
+        name: 'Publikasi',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'DataDasar',
+        component: DataDasarView,
+        name: 'DataDasar',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'Aspek',
+        component: AspekView,
+        name: 'Aspek',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'MasterIndikator',
+        component: MasterIndikatorView,
+        name: 'MasterIndikator',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'JenisSatker',
+        component: JenisSatkerView,
+        name: 'JenisSatker',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'Periode',
+        component: PeriodeView,
+        name: 'Periode',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'Indikator_Periode',
+        component: IndikatorPeriodeView,
+        name: 'Indikator_Periode',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'SatuanKerja',
+        component: SatuanKerjaView,
+        name: 'SatuanKerja',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'Capaian_Unit',
+        component: CapaianUnitView,
+        name: 'Capaian_Unit',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'Indikator_SatuanKerja',
+        component: IndikatorSatuanKerjaView,
+        name: 'Indikator_SatuanKerja',
+        meta: {description: 'Simple and advance table in CoPilot'}
+      }, {
+        path: 'Indikator_SatuanKerja_Log',
+        component: IndikatorSatuanKerjaLogView,
+        name: 'Indikator_SatuanKerja_Log',
         meta: {description: 'Simple and advance table in CoPilot'}
       }, {
         path: 'tasks',
@@ -62,32 +141,7 @@ const routes = [
         component: ReposView,
         name: 'Repository',
         meta: {description: 'List of popular javascript repos'}
-      }, {
-        path: 'Excel',
-        component: ExcelView,
-        name: 'Excel',
-        meta: {description: 'List of Excel'}
-      },{
-        path: 'DataDasar',
-        component: DataDasarView,
-        name: 'Data Dasar',
-        meta: {description: ''}
-      },{
-        path: 'KategoriUnit',
-        component: KategoriUnitView,
-        name: 'Kategori Unit',
-        meta: {description: 'l'}
-      },{
-        path: 'Unit',
-        component: UnitView,
-        name: 'Unit',
-        meta: {description: ''}
-      },{
-        path: 'CapaianUnit',
-        component: CapaianUnitView,
-        name: 'Capaian Unit',
-        meta: {description: ''}
-      }  
+      }
     ]
   }, {
     // not found handler
