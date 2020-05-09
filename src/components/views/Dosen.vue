7863f9707ab72732e1f944133b590986a46e0c15
ilhamsenaa <55700487+ilhamsenaa@users.noreply.github.com>
2020-05-09 21:55:10 +0700

    Update Dosen.vue

--- src/components/views/Dosen.vue
+++ src/components/views/Dosen.vue
@@ -1,78 +1,27 @@
 <template>
-  <div>
-    <div id="app" ref="spreadsheet"></div>
-    <div>
-        <input type="button" value="Add New Row" @click="() => spreadsheet.insertRow()" />
-        <input type="button" value="Delete Selected Row" @click="() => spreadsheet.deleteRow()" />
-    </div>
-  </div>
+    <div>The component will appear here</div>
 </template>
-
 <script>
-import jexcel from 'jexcel'
-import 'jexcel/dist/jexcel.css'
 import axios from 'axios'
+var host = 'http://10.199.14.46:8003/'
+import WebDataRocks from 'webdatarocks'
 export default {
-  // name: 'App',
-  data() {
-    return {
-      Aspek: [],
-      form: {
-        id: '',
-        aspek: '',
-        komponen_aspek: ''
-      }
-    }
-  },
-  mounted() {
-    let spreadsheet = jexcel(this.$el, this.jexcelOptions)
-    Object.assign(this, { spreadsheet })
-  },
-  methods: {
-    newRow() {
-      axios.post('http://10.199.14.46:8003/api/Aspek/', this.form).then(res => {
-        console.log(res.data)
+  mounted: function() {
+    axios.get(host + 'api/Dosen/').then(res => {
+      console.log(res.data)
+      this.webdatarocks = new WebDataRocks({
+        report: { data: res.data },
+        container: this.$el
       })
-    },
-    updateRow(instance, cell, columns, row, value) {
-      axios.get('http://10.199.14.46:8003/api/Aspek/').then(res => {
-        var index = Object.values(res.data[row])
-        index[columns] = value
-        console.log(index)
-        axios.put('http://10.199.14.46:8003/api/Aspek/' + index[0], {
-          id: index[0],
-          aspek: index[1],
-          komponen_aspek: index[2]
-        }).then(res => {
-          console.log(res.data)
-        })
-      })
-    },
-    deleteRow(instance, row) {
-      axios.get('http://10.199.14.46:8003/api/Aspek/').then(res => {
-        var index = Object.values(res.data[row])
-        // console.log(index)
-        console.log(row)
-        axios.delete('http://10.199.14.46:8003/api/Aspek/' + index[0])
-      })
-    }
+    })
   },
-  computed: {
-    jexcelOptions() {
-      return {
-        data: this.Aspek,
-        allowToolbar: true,
-        url: 'http://10.199.14.46:8003/api/Aspek/',
-        onchange: this.updateRow,
-        oninsertrow: this.newRow,
-        ondeleterow: this.deleteRow,
-        columns: [
-          { type: 'text', title: 'id', width: '50px' },
-          { type: 'text', title: 'aspek', width: '200px' },
-          { type: 'text', title: 'komponen_aspek', wordWrap: true, width: '200px' }
-        ]
-      }
-    }
+  beforeUpdate() {
+    return false
   }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import '~webdatarocks/webdatarocks.min.css';
+</style>
